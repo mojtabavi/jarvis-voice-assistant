959abbcb488d3c9d9ea3585cc48fccc1a1065c87
Ali <mojtabavi99@yahoo.com>
2020-07-13 14:21:19 +0430

    load plugins in main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from assistant import assistant
 import config
+import importlib
+import traceback
+
+for plug in config.PLUGINS:
+    try:
+        print(f"Loading {plug} plugin ...")
+        importlib.import_module(f'{config.PLUGINS_DIR}.{plug}')
+    except Exception:
+        traceback.print_exc()
