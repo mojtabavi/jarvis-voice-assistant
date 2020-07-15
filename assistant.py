2998d01ca440e132e22dd73190abf61930d5b917
Ali <mojtabavi99@yahoo.com>
2020-07-15 18:40:50 +0430

    move assistant modules to assistant folder

--- assistant.py
+++
@@ -1,8 +0,0 @@
-from jarvis import Jarvis
-import config
-
-assistant = Jarvis(
-                vosk_model=config.VOSK_MODEL,
-                hotword_models=config.HOTWORD_MODELS,
-                sensitivity=config.SENSITIVITY,
-                command_mode_time=config.COMMAND_MODE_DURATION)
