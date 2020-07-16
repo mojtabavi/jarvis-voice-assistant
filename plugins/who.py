618973a91c36c316cc8c7a33df37176ac2d5bdd0
Ali <mojtabavi99@yahoo.com>
2020-07-16 16:20:19 +0430

    fix some bugs

--- plugins/who.py
+++ plugins/who.py
@@ -12,7 +12,6 @@ who are you?          jarvis introduce
 
 @assistant.register_handler(filters.equals("what's your name"))
 @assistant.register_handler(filters.contains('name'))
-@assistant.register_handler(filters.regexp(r'name'))
 def intro_cmd_mode(cmd):
     utils.play('jarvis-intro.wav')
     
