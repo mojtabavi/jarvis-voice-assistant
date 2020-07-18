c6d8fb345ba45b09eb462ffd3afd195ada54dbd6
Ali <mojtabavi99@yahoo.com>
2020-07-19 00:02:34 +0430

    add config file to project

---
+++ config.py
@@ -0,0 +1,20 @@
+VOSK_MODEL = "assets/models/vosk-api"
+
+HOTWORD_MODELS = [
+    "assets/models/JARVIS.pmdl"
+]
+
+SENSITIVITY = 0.5
+
+COMMAND_MODE_DURATION = 40
+
+PLUGINS_DIR = "plugins"
+
+
+AUDIOS_DIR ="assets/audios"
+
+PLUGINS = [
+    "alarm",
+    "main",
+    "who",
+]
